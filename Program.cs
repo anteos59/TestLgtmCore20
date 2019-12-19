03156ff6d4984c6f950cfaefd24074d4172ff5b3
Andreas Teizel <andreas.teizel@posteo.de>
2019-12-20 00:06:53 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLgtmCore20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test LGTM Dotnet Core 2.0");
+        }
+    }
+}
